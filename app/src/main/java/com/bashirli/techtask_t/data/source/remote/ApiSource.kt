2de2677e2f785a2a038830399bf351b59c993154
Bashirli <bashirli1903@gmail.com>
2023-12-18 17:09:37 +0400

    initial

--- app/src/main/java/com/bashirli/techtask_t/data/source/remote/ApiSource.kt
+++ app/src/main/java/com/bashirli/techtask_t/data/source/remote/ApiSource.kt
@@ -1,7 +1,6 @@
 package com.bashirli.techtask_t.data.source.remote
 
 import com.bashirli.techtask_t.common.Resource
-import com.bashirli.techtask_t.data.dto.local.PeopleEntityDTO
 import com.bashirli.techtask_t.data.dto.remote.CountryDTO
 
 interface ApiSource {
